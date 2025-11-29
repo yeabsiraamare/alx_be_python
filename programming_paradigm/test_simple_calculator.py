4d2e9b65c43a3e79f78a7f721a7eafc14df68af2
yeabsiraamare <yeabsiraamaree@gmail.com>
2025-11-29 20:33:24 +0300

    added a python program that does unit testing on basic mathmathics operations

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -20,13 +20,13 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(3,0),3)
         self.assertEqual(self.calc.subtract(-3,-5),-8)
     
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(5,2),10)
         self.assertEqual(self.calc.multiply(5,0),0)
         self.assertEqual(self.calc.multiply(-6,2),-12)
         self.assertEqual(self.calc.multiply(-7,-3),21)
     
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10,2),5)
         self.assertEqual(self.calc.divide(0,4),0)
         self.assertEqual(self.calc.divide(8,0),None)
